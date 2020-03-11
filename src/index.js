4011903a158680dd84526fcccddf445cf9247aef
imshaz <imshazbangash@gmail.com>
2020-03-11 16:54:49 -0400

    updates

--- src/index.js
+++ src/index.js
@@ -1,8 +1,28 @@
-const { GraphQLServer } = require('graphql-yoga')
+const { GraphQLServer } = require("graphql-yoga");
+const users =[
+    {
+        id:'123', 
+        name:'Shaz', 
+        email:'shaz@gmail.com', 
+        age:29
+    }, 
+    {
+        id:'124', 
+        name:'sara', 
+        email:'sara@gmail.com', 
+        age:28
+    }, 
+    {id:'125', 
+        name:'ali', 
+        email:'ali@hotmail.com', 
+        age:30
+    }
 
-const typeDefs =`
+]
+const typeDefs = `
 type Query {
-   greeting(name:String):String!
+    user(query:String):User
+   users:[User!]!
    me:User!
    post: Post!
 }
@@ -10,7 +30,7 @@ type User{
     id:ID!
     name:String! 
     email:String!
-    contact:String!
+    age:String!
 }
 
 type Post {
@@ -19,30 +39,47 @@ type Post {
     body:String!
     published:Boolean!
 }
-`
+`;
 
-const resolvers ={
-    Query:{
-        
-        me(){
-            return {
-                id:'sjslk',
-                name:'shaz',
-                email:'shaz@me.com',
-                contact:'00923009159999'
-            }
-        },
-    
-        post(){
-            return {
-                id:'id-1',
-                title:'graphql',
-                body:'Graphql is fun',
-                published:false
-            }
-        }
-    }
-}
+const resolvers = {
+  Query: {
+      users(parent, args, ctx, info){
 
-const server =new GraphQLServer({typeDefs, resolvers})
-server.start(()=>{console.log("server running on localhost:4000")})
+return users
+      },
+      user(parent, args, ctx, info){
+
+        // if(!args.query) return null
+
+// TODO
+        return users.filter(user=>{
+            // const 
+          return user.name.toLowerCase().includes(args.query.toLowerCase());
+        })
+      },
+    me() {
+      return {
+        id: "sjslk",
+        name: "shaz",
+        email: "shaz@me.com",
+        age: 29
+      };
+    },
+
+    post() {
+      return {
+        id: "id-1",
+        title: "graphql",
+        body: "Graphql is fun",
+        published: false
+      };
+    },
+
+ 
+  }
+};
+
+const server = new GraphQLServer({ typeDefs, resolvers });
+server.start(() => {
+  console.log("server running on localhost:4000");
+});
