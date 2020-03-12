7ed8fa7dedbe18cb2ae1fa4a4af036b14a9bf8bd
imshaz <imshazbangash@gmail.com>
2020-03-12 13:52:52 -0400

    added spread operator and updated code to be more readable

--- src/index.js
+++ src/index.js
@@ -167,9 +167,7 @@ const resolvers = {
 
       const user={
       id : uuidv4(),
-      name:args.name,
-      email:args.email,
-      age:args.age
+      ...args
 
       }
 
@@ -190,10 +188,7 @@ const resolvers = {
 
       const post ={
         id:uuidv4(), 
-        title:args.title, 
-        body:args.body, 
-        published:args.published,
-        author:args.author
+      ...args
       }
       posts.push(post)
       return post
@@ -216,9 +211,7 @@ const resolvers = {
 
     const comment ={
       id:uuidv4(),
-      text:args.text, 
-      author:args.author, 
-      post:args.post
+      ...args
     }
 
     comments.push(comment);
